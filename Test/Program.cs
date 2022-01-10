bfd19191535ac11c87879dfdef0e3e0582fe4442
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2022-01-10 11:13:16 -0600

    Added Test project (console app).

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using MikisColorTools;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color color = Color.LightCoral;
+            Color compColor = IMikisColorTools.GetComplementaryColor(color);
+
+            Console.WriteLine(string.Format("LightCoral: ({0}, {1}, {2}, {3})", color.R, color.G, color.B, color.A));
+
+            Console.WriteLine(string.Format("Complementary: ({0}, {1}, {2}, {3})", compColor.R, compColor.G, compColor.B, compColor.A));
+
+            Console.ReadKey();
+        }
+    }
+}
