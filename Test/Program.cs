2f7d103b40c1089ee99187ce66433462d8a64f51
RachaelMiki1215 <rachaelbuxton1215@gmail.com>
2022-01-10 23:59:48 -0600

    Added test code.

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,11 +9,21 @@ namespace Test
         static void Main(string[] args)
         {
             Color color = Color.LightCoral;
-            Color compColor = IMikisColorTools.GetComplementaryColor(color);
-
             Console.WriteLine(string.Format("LightCoral: ({0}, {1}, {2}, {3})", color.R, color.G, color.B, color.A));
 
+            Color compColor = IMikisColorTools.GetComplementaryColor(color);
             Console.WriteLine(string.Format("Complementary: ({0}, {1}, {2}, {3})", compColor.R, compColor.G, compColor.B, compColor.A));
+
+            Console.WriteLine(IMikisColorTools.C(color));
+            Console.WriteLine(IMikisColorTools.M(color));
+            Console.WriteLine(IMikisColorTools.Y(color));
+            Console.WriteLine(IMikisColorTools.K(color));
+
+            Color[] triadicColors = IMikisColorTools.GetNEvenlySpacedColorScheme(Color.LightGoldenrodYellow, 3);
+            foreach (Color c in triadicColors)
+            {
+                Console.WriteLine(string.Format("Triadic: ({0}, {1}, {2})", c.R, c.G, c.B));
+            }
 
             Console.ReadKey();
         }
